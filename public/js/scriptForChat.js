4e197fcb5c805a13c5b223b24e770f0d6219df6a
bhavinsa <bhavinpatelsa@gmail.com>
2019-08-23 15:54:14 +0530

    Javascript window close method code added

--- public/js/scriptForChat.js
+++ public/js/scriptForChat.js
@@ -1,5 +1,4 @@
 $(function() {
-  var loginPage = "http://localhost:5000/user/login";
   var socket = io("/chat");
 
   var username = $("#user").val();
@@ -19,8 +18,13 @@ $(function() {
         // Logout user
         // alert("This user going to logout.");
         console.log("Logout User here");
+        window.close();
         // window.location.href = loginPage;
       }
+
+      //document.getElementById("hell0").innerHTML += '<li>'+ data.description +'</li>';
+      // $('#hell0').append($('<li>').append($(data.description).append($('<li>');
+      //$('#hell0').scrollTop($('#hell0')[0].scrollHeight);
     });
   }); //end of connect event.
 
@@ -29,30 +33,33 @@ $(function() {
     $("#list").empty();
     $("#list").append(
       $("<li>").append(
-        $('<button id="ubtn" class="btn btn-info btn-block"></button>')
-          .text("Click here to chat in Group")
-          .css({ "font-size": "12px" })
+        $('<button id="ubtn" class="btn btn-danger btn-block btn-lg"></button>')
+          .text("Group")
+          .css({ "font-size": "18px" })
       )
     );
     var totalOnline = 0;
     for (var user in stack) {
-      if (user == username) {
-        return true;
-      }
       //setting txt1. shows users button.
-      var txt1 = $('<button id="ubtn" class="btn btn-success  btn-md">')
-        .text(user)
-        .css({ "font-size": "14px" });
+      if (user == username) {
+        var txt1 = $('<button class="boxF disabled"> </button>')
+          .text(user)
+          .css({ "font-size": "18px" });
+      } else {
+        var txt1 = $('<button id="ubtn" class="btn btn-success  btn-md">')
+          .text(user)
+          .css({ "font-size": "18px" });
+      }
       //setting txt2. shows online status.
       if (stack[user] == "Online") {
         var txt2 = $('<span class="badge"></span>')
-          .text(stack[user])
-          .css({ float: "right", color: "#009933", "font-size": "14px" });
+          .text("*" + stack[user])
+          .css({ float: "right", color: "#009933", "font-size": "18px" });
         totalOnline++;
       } else {
         var txt2 = $('<span class="badge"></span>')
           .text(stack[user])
-          .css({ float: "right", color: "#a6a6a6", "font-size": "14px" });
+          .css({ float: "right", color: "#a6a6a6", "font-size": "18px" });
       }
       //listing all users.
       $("#list").append($("<li>").append(txt1, txt2));
@@ -139,11 +146,11 @@ $(function() {
             .css({ color: "#006080" });
           var txt2 = $("<span></span>")
             .text(chatDate)
-            .css({ float: "right", color: "#a6a6a6", "font-size": "10px" });
+            .css({ float: "right", color: "#a6a6a6", "font-size": "14px" });
           var txt3 = $("<p></p>").append(txt1, txt2);
           var txt4 = $("<p></p>")
             .text(data.result[i].msg)
-            .css({ color: "#000000", "font-size": "12px" });
+            .css({ color: "#000000" });
           //showing chat in chat box.
           $("#messages").prepend($("<li>").append(txt3, txt4));
           msgCount++;
@@ -173,6 +180,19 @@ $(function() {
     }
   }); //end of keyup handler.
 
+  //receiving typing message.
+  socket.on("typing", function(msg) {
+    var setTime;
+    //clearing previous setTimeout function.
+    clearTimeout(setTime);
+    //showing typing message.
+    $("#typing").text(msg);
+    //showing typing message only for few seconds.
+    setTime = setTimeout(function() {
+      $("#typing").text("");
+    }, 3500);
+  }); //end of typing event.
+
   //sending message.
   $("form").submit(function() {
     socket.emit("chat-msg", {
@@ -194,11 +214,11 @@ $(function() {
       .css({ color: "#006080" });
     var txt2 = $("<span></span>")
       .text(chatDate)
-      .css({ float: "right", color: "#a6a6a6", "font-size": "12px" });
+      .css({ float: "right", color: "#a6a6a6", "font-size": "14px" });
     var txt3 = $("<p></p>").append(txt1, txt2);
     var txt4 = $("<p></p>")
       .text(data.msg)
-      .css({ color: "#000000", "font-size": "12px" });
+      .css({ color: "#000000" });
     //showing chat in chat box.
     $("#messages").append($("<li>").append(txt3, txt4));
     msgCount++;
